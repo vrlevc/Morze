106ca708885cb638b7243eaa9766caceae1951bf
glvlevch <viktor.levchenko@avid.com>
2018-09-07 17:39:16 +0300

    Initial layout with input and output views for text and more code.

--- Morze/ViewController.swift
+++ Morze/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextViewDelegate {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -19,7 +19,59 @@ class ViewController: UIViewController {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
-
-
+	
+	// MARK: - UI Elements
+	@IBOutlet weak var inputTextView: UITextView!
+	@IBOutlet weak var outputMorzeView: UITextView!
+		
+	// MARK: - UITextViewDelegate
+	
+	func textViewShouldBeginEditing(_ textView: UITextView) -> Bool {
+		if textView == inputTextView {
+			outputMorzeView.text.append("textViewShouldBeginEditing \n")
+		}
+		return true;
+	}
+	
+	func textViewShouldEndEditing(_ textView: UITextView) -> Bool {
+		return true;
+	}
+	
+	func textViewDidBeginEditing(_ textView: UITextView) {
+		
+	}
+	
+	func textViewDidEndEditing(_ textView: UITextView) {
+		
+	}
+	
+	func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+		return true;
+	}
+	
+	func textViewDidChange(_ textView: UITextView) {
+		
+	}
+	
+	func textViewDidChangeSelection(_ textView: UITextView) {
+		
+	}
+	
+	func textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
+		return true;
+	}
+	
+	func textView(_ textView: UITextView, shouldInteractWith textAttachment: NSTextAttachment, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
+		return true;
+	}
+	
+	func textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange) -> Bool {
+		return true;
+	}
+	
+	func textView(_ textView: UITextView, shouldInteractWith textAttachment: NSTextAttachment, in characterRange: NSRange) -> Bool {
+		return true;
+	}
+	
 }
 
